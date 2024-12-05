adfcbf26a6e79622ae508f1b0dc4e869d14d3e0e
Malpercio <root@malpercio.dev>
2024-12-04 18:08:40 -0600

    chore: did I break it with the did json?

--- src/labeler.ts
+++ src/labeler.ts
@@ -9,40 +9,39 @@ import {
 } from "./constants.js";
 import { LabelerServer } from "@skyware/labeler";
 
-console.log("creating labeler server")
 const server = new LabelerServer({
   did: DID,
   signingKey: SIGNING_KEY,
   dbPath: "./data/labels.db",
 });
-server.app.get('/.well-known/did.json', (req, res) => {
-  res.send({
-    "@context": [
-      "https://www.w3.org/ns/did/v1",
-      "https://w3id.org/security/multikey/v1",
-      "https://w3id.org/security/suites/secp256k1-2019/v1"
-    ],
-    "id": "did:web:brightershores.malpercio.dev",
-    "alsoKnownAs": [
-      "at://brightershores.malpercio.dev"
-    ],
-    "verificationMethod": [
-      {
-        "id": "did:web:brightershores.malpercio.dev#atproto",
-        "type": "Multikey",
-        "controller": "did:web:brightershores.malpercio.dev",
-        "publicKeyMultibase": "zDnaejUT7PJpdrFFt9ccGohj3L78TwzeGcku5cEpqtVCn4NRt"
-      }
-    ],
-    "service": [
-      {
-        "id": "#atproto_pds",
-        "type": "AtprotoPersonalDataServer",
-        "serviceEndpoint": "https://pds.malpercio.dev"
-      }
-    ]
-  })
-})
+// server.app.get('/.well-known/did.json', (req, res) => {
+//   res.send({
+//     "@context": [
+//       "https://www.w3.org/ns/did/v1",
+//       "https://w3id.org/security/multikey/v1",
+//       "https://w3id.org/security/suites/secp256k1-2019/v1"
+//     ],
+//     "id": "did:web:brightershores.malpercio.dev",
+//     "alsoKnownAs": [
+//       "at://brightershores.malpercio.dev"
+//     ],
+//     "verificationMethod": [
+//       {
+//         "id": "did:web:brightershores.malpercio.dev#atproto",
+//         "type": "Multikey",
+//         "controller": "did:web:brightershores.malpercio.dev",
+//         "publicKeyMultibase": "zDnaejUT7PJpdrFFt9ccGohj3L78TwzeGcku5cEpqtVCn4NRt"
+//       }
+//     ],
+//     "service": [
+//       {
+//         "id": "#atproto_pds",
+//         "type": "AtprotoPersonalDataServer",
+//         "serviceEndpoint": "https://pds.malpercio.dev"
+//       }
+//     ]
+//   })
+// })
 server.start(PORT, (error, address) => {
   if (error) console.error(error);
   else console.log(`Labeler server listening on ${address}`);
